6c3c0ce6c4cde991834e8156427d2d563653a269
johnjohn123456 <johnaf123456@gmail.com>
2018-04-02 18:45:46 +0200

    some changes

--- client/youtube-app/src/components/video_detail.js
+++ client/youtube-app/src/components/video_detail.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../App.css'
-import logo from '../loading-gif-transparent-4.gif'
+import '../style/App.css'
+import logo from '../img/loading-gif-transparent-4.gif'
 
 const VideoDetail = (props) => {
   if (!props.video) {
